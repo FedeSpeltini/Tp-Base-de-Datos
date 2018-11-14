71fcb88e8918ca24a781b41bf5c3d405f7c1a680
fspeltini <44760275+fspeltini@users.noreply.github.com>
2018-11-14 17:04:04 -0300

    Create intento15.sql

---
+++ intento15.sql
@@ -0,0 +1,25 @@
+SELECT t1.Mes, ( SELECT QueDias FROM tuTabla t2 WHERE t1.Mes = t2.Mes ORDER BY t1.Mes FOR XML PATH('') ) AS Dias 
+FROM tuTabla t1 GROUP BY t1.Mes ;
+
+
+SELECT QueDias FROM tuTabla t2 WHERE t1.Mes = t2.Mes ORDER BY t1.Mes FOR XML PATH('')
+
+
+(select carrera.nombre from carrera where carrera_usuario.id_carrera = carrera.id ORDER BY carrera_usuario.id_carrera FOR XML PATH(''))
+
+(select carrera.nombre from carrera where carrera_usuario.id_carrera = carrera.id ORDER BY carrera_usuario.id_carrera  string_agg(''))
+
+																																  
+(select carrera.nombre from carrera where carrera_usuario.id_carrera = carrera.id ORDER BY carrera_usuario.id_carrera  string_agg(''))																																  
+(select	string_agg(carrera.nombre, ',')																														  
+-- vInactivo.nombre, apellido, username, email,
+ 
+ 
+select vInactivo.id, vInactivo.nombre, apellido, username, email, ((select string_agg(carrera.nombre, '') where carrera_usuario.id_carrera = carrera.id) from vInactivo
+INNER JOIN carrera_usuario ON vInactivo.id = carrera_usuario.id_usuario
+INNER JOIN carrera ON carrera_usuario.id_carrera = carrera.id
+group by vInactivo.id;
+																   
+																   
+select string_agg(Name, ',') as Names
+from T
