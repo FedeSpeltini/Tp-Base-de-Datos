e4dcb7c343fed99183e5dcf093e1e47bac7f19aa
fspeltini <44760275+fspeltini@users.noreply.github.com>
2018-11-16 14:49:36 -0300

    Update ejercicio14.sql

--- ejercicio14.sql
+++ ejercicio14.sql
@@ -1,3 +1,12 @@
 select DISTINCT(usuario.id), contenido, fecha_comentario from comentario 
 INNER JOIN usuario ON usuario.id = comentario.id_user
 order by fecha_comentario desc
+
+
+
+
+select usuario.id, usuario.nombre, contenido, fecha_publicacion 
+from   publicacion p 
+INNER JOIN usuario ON usuario.id = p.id_user
+where  fecha_publicacion = (select max(fecha_publicacion) from publicacion where publicacion.id_user = p.id_user)
+order by usuario.id, fecha_publicacion desc
