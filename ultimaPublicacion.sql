1a413fe775dc015b38e236500b616bee9265acd3
fspeltini <44760275+fspeltini@users.noreply.github.com>
2018-11-18 09:59:18 -0300

    Update ultimaPublicacion.sql

--- ultimaPublicacion.sql
+++ ultimaPublicacion.sql
@@ -3,3 +3,16 @@ from   publicacion p
 INNER JOIN usuario ON usuario.id = p.id_user
 where  fecha_publicacion = (select max(fecha_publicacion) from publicacion where publicacion.id_user = p.id_user)
 order by usuario.id, fecha_publicacion desc
+
+
+
+
+
+
+
+create view vUltimaPublicacionUsuario as
+select usuario.username, contenido, fecha_publicacion 
+from   publicacion p 
+INNER JOIN usuario ON usuario.id = p.id_user
+where  fecha_publicacion = (select max(fecha_publicacion) from publicacion where publicacion.id_user = p.id_user)
+order by usuario.id, fecha_publicacion desc
